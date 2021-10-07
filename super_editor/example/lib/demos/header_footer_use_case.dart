8e15bab3238d35bb2b3991fb219cb9276a9dff81
Brian Egan <brian@superlist.com>
2021-10-07 14:17:42 +0200

    Update use case with sticky footer

--- super_editor/example/lib/demos/header_footer_use_case.dart
+++ super_editor/example/lib/demos/header_footer_use_case.dart
@@ -1,3 +1,4 @@
+import 'package:example/demos/sliver_stick_to_bottom.dart';
 import 'package:flutter/material.dart';
 import 'package:super_editor/super_editor.dart';
 
@@ -83,36 +84,38 @@ class _HeaderFooterUseCaseState extends State<HeaderFooterUseCase> {
             ),
           ),
         ),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            const Text(
-                'Headers/Footers may contain Text fields and buttons. Here, we have a "messaging experience"'),
-            for (final message in _messages)
-              ListTile(
-                title: Text(message),
-              ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter message...',
+        SliverStickToBottom(
+          child: Column(
+            children: [
+              const Text(
+                  'Headers/Footers may contain Text fields and buttons. Here, we have a "messaging experience" that sticks to the bottom of the screen if the document isn\'t large enough, or scrolls with the document if the document is large enough'),
+              for (final message in _messages)
+                ListTile(
+                  title: Text(message),
+                ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter message...',
+                  ),
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (_controller.text.isNotEmpty) {
-                    _messages.add(_controller.text);
-                  }
-                });
-              },
-              child: const Text("Send message"),
-            ),
-          ]),
-        ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (_controller.text.isNotEmpty) {
+                      _messages.add(_controller.text);
+                    }
+                  });
+                },
+                child: const Text("Send message"),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
@@ -136,33 +139,6 @@ Document _createInitialDocument() {
         text: AttributedText(
           text:
               'Document should take up as much space as it needs. (No scroll within a scroll, which can happen depending on the size of this window). Document should be surrounded by interactive / "complex" headers and footers. Users need to be able to interact with Buttons and Text fields as they normally would.',
-        ),
-      ),
-      ParagraphNode(
-        id: DocumentEditor.createNodeId(),
-        text: AttributedText(
-          text:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed sagittis urna. Aenean mattis ante justo, quis sollicitudin metus interdum id. Aenean ornare urna ac enim consequat mollis. In aliquet convallis efficitur. Phasellus convallis purus in fringilla scelerisque. Ut ac orci a turpis egestas lobortis. Morbi aliquam dapibus sem, vitae sodales arcu ultrices eu. Duis vulputate mauris quam, eleifend pulvinar quam blandit eget.',
-        ),
-      ),
-      ParagraphNode(
-        id: DocumentEditor.createNodeId(),
-        text: AttributedText(
-            text:
-                'Cras vitae sodales nisi. Vivamus dignissim vel purus vel aliquet. Sed viverra diam vel nisi rhoncus pharetra. Donec gravida ut ligula euismod pharetra. Etiam sed urna scelerisque, efficitur mauris vel, semper arcu. Nullam sed vehicula sapien. Donec id tellus volutpat, eleifend nulla eget, rutrum mauris.'),
-      ),
-      ParagraphNode(
-        id: DocumentEditor.createNodeId(),
-        text: AttributedText(
-          text:
-              'Nam hendrerit vitae elit ut placerat. Maecenas nec congue neque. Fusce eget tortor pulvinar, cursus neque vitae, sagittis lectus. Duis mollis libero eu scelerisque ullamcorper. Pellentesque eleifend arcu nec augue molestie, at iaculis dui rutrum. Etiam lobortis magna at magna pellentesque ornare. Sed accumsan, libero vel porta molestie, tortor lorem eleifend ante, at egestas leo felis sed nunc. Quisque mi neque, molestie vel dolor a, eleifend tempor odio.',
-        ),
-      ),
-      ParagraphNode(
-        id: DocumentEditor.createNodeId(),
-        text: AttributedText(
-          text:
-              'Etiam id lacus interdum, efficitur ex convallis, accumsan ipsum. Integer faucibus mollis mauris, a suscipit ante mollis vitae. Fusce justo metus, congue non lectus ac, luctus rhoncus tellus. Phasellus vitae fermentum orci, sit amet sodales orci. Fusce at ante iaculis nunc aliquet pharetra. Nam placerat, nisl in gravida lacinia, nisl nibh feugiat nunc, in sagittis nisl sapien nec arcu. Nunc gravida faucibus massa, sit amet accumsan dolor feugiat in. Mauris ut elementum leo.',
         ),
       ),
     ],
